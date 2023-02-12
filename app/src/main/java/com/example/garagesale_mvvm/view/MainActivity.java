cb6ff9aa83f26c218767c44398328a4bcbb461b6
lindamol <lindamoljosem@gmail.com>
2023-02-12 00:26:00 -0500

    Initial commit

---
+++ app/src/main/java/com/example/garagesale_mvvm/view/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.garagesale_mvvm.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.garagesale_mvvm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void EmailLogin(View view) {
+    }
+
+    public void Forgotpassword(View view) {
+    }
+
+    public void NewRegister(View view) {
+    }
+}
